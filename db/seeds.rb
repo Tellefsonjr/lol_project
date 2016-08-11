63031e60216aa20983e202d99b67206b92b441bf
Tellefsonjr <Timmeyellefson@gmail.com>
2016-08-11 15:25:38 -0700

    seeds logins for devs, follows eachother

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+timmey = User.create!(name: "Timmey", email: "timmeyellefson@gmail.com",
+  password: "teejteej123", password_confirmation: "teejteej123")
+clark = User.create!(name: "Clark", email: "supermoneezzy@gmail.com",
+  password: "clark123", password_confirmation: "clark123")
+jerico = User.create!(name: "Jerico", email: "jericomanalo@gmail.com",
+  password: "jerico123", password_confirmation: "jerico123")
+
+timmey.follow(clark)
+timmey.follow(jerico)
+clark.follow(timmey)
+clark.follow(jerico)
+jerico.follow(timmey)
+jerico.follow(clark)
